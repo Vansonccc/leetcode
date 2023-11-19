4739c8859a7602261032bd4c7900fceff2781903
Wanxuan Chen <413155202@qq.com>
2023-11-19 22:09:51 +0800

    2023/11/19
    2023/11/19
    
    Co-Authored-By: Vanson <3021140+vanson@users.noreply.github.com>

---
+++ leetcodeJava/src/main/java/200/250-260/Leetcode258.java
@@ -0,0 +1,28 @@
+public class Leetcode258 {
+    public static void main(String[] args) {
+        System.out.println(addDigits(38));
+    }
+
+    public static int addDigits(int num) {
+        while (String.valueOf(num).length() > 1) {
+            int temp = 0;
+            for (int i = 0; i < String.valueOf(num).length(); i++) {
+                temp += Integer.parseInt(String.valueOf(String.valueOf(num).charAt(i)));
+            }
+            num = temp;
+        }
+        return num;
+    }
+
+    public int addDigits2(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
